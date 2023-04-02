da7bfd3cd38bfc2c385a983ee6ba49ec975bab6e
mecha <alpha12345op@gmail.com>
2023-04-02 02:17:59 +0100

    PM command is finished, using "Adventure"'s Component system for sending messages to clients!

--- src/main/java/me/maikeru/hello_world/CommandPM.java
+++ src/main/java/me/maikeru/hello_world/CommandPM.java
@@ -1,5 +1,12 @@
 package me.maikeru.hello_world;
 
+import com.google.gson.Gson;
+import net.kyori.adventure.text.BuildableComponent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentBuilder;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -7,44 +14,65 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.checkerframework.checker.units.qual.C;
 import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
+
+import java.io.Console;
 
 public class CommandPM implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, String command_name, String[] strings) {
-        String name = strings[0];
-        if (name == null || name.equals("")) {
-            commandSender.sendMessage(ChatColor.RED + "Who are you sending your message to? Usage: /pm (name) (message...)");
+        String receiverName = strings[0];
+        if (receiverName == null) { // 1. Validation
+            commandSender.sendMessage(
+                    ChatColor.RED + "Who are you sending your message to? Usage: /pm (name) (message...)");
             return false;
-        } else if (strings == null || strings.length == 1 ) {
-            commandSender.sendMessage(ChatColor.RED + "What is your message? Usage: /pm (name) (message...)");
+        } else if (strings.length <= 1 ) {
+            commandSender.sendMessage(
+                    ChatColor.RED + "What is your message? Usage: /pm (name) (message...)");
             return false;
-        } else if (Bukkit.getPlayer(name) == null || name.equalsIgnoreCase("console")) {
-            commandSender.sendMessage(ChatColor.RED + "That player isn't online right now, check your spelling. Usage: /pm (name) (message...)");
+        } else if (!receiverName.equalsIgnoreCase("console") && Bukkit.getPlayer(receiverName) == null) {
+            commandSender.sendMessage(
+                    ChatColor.RED + "That player isn't online right now, check your spelling? Usage: /pm (name) (message...)");
             return false;
         }
-        String message = compileStrArray(strings, 1);
+        String msg_content = compileStrArray(strings, 1); // String.join isn't usable here.
 
-        if (commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-            player.sendMessage(ChatColor.GRAY + "" + player.displayName().toString() + " -> " + name + " : " + message);
-            if (name.equalsIgnoreCase("console")) { // intended receiver is console
-                Bukkit.getLogger().info(ChatColor.GRAY + "" + player.displayName().toString() + " -> Console : " + message);
-            }else {
-                Player receiver = Bukkit.getPlayer(name);
-                receiver.sendMessage(ChatColor.GRAY + "" + player.displayName().toString() + " -> " + receiver.displayName().toString() + " : " + message);
-            }
+        TextComponent senderDisplayName = null;
+        if (commandSender instanceof ConsoleCommandSender) {
+            senderDisplayName = Component.text("Console")
+                    .color(TextColor.color(61, 235, 52));
+        } else if (commandSender instanceof Player) {
+            senderDisplayName = (TextComponent) ((Player) commandSender).displayName();
+        } else return false;
 
-        }else if (commandSender instanceof ConsoleCommandSender) {
-            ConsoleCommandSender console = (ConsoleCommandSender) commandSender;
-            console.sendMessage(ChatColor.GRAY + "Console -> " + name + " : " + message);
+        TextComponent receiverDisplayName = null;
+        if (receiverName.equalsIgnoreCase("console")){
+            receiverDisplayName = Component.text("Console")
+                    .color(TextColor.color(61, 235, 52));
+        } else {
+            receiverDisplayName = (TextComponent) Bukkit.getPlayer(receiverName).displayName();
+        }
 
-            if (name.equalsIgnoreCase("console")) {
-                Bukkit.getLogger().info(ChatColor.GRAY + "Console -> Console : " + message);
-            }else {
-                Player receiver = Bukkit.getPlayer(name);
-                receiver.sendMessage(ChatColor.GRAY + "Console -> " + receiver.displayName().toString() + " : " + message);
-            }
+        final TextComponent msg = Component.text()
+                .append(senderDisplayName)
+                .append(Component.text().content(" -> ").color(TextColor.color(128,128,128)))
+                .append(receiverDisplayName)
+                .append(Component.text(" : " + msg_content).color(TextColor.color(128,128,128)))
+                .build();
+
+        if(receiverName.equalsIgnoreCase("console")) { // Send the msg to receiver
+            Bukkit.getLogger().info(
+                    senderDisplayName.content() + " -> Console : " + msg_content);
+        }else {
+            Bukkit.getPlayer(receiverName).sendMessage(msg);
+        }
+
+        if (commandSender instanceof Player ) {
+            commandSender.sendMessage(msg);
+        } else {
+            Bukkit.getLogger().info("Console -> " + receiverDisplayName.content() + " : " + msg_content);
         }
 
         return true;
